c0bed176ea350f5fbb1bbfdc04db0730eb8d5fc2
Claudio Contardo <ccontard@maradona.gerad.lan>
2019-05-22 09:59:48 -0400

    -> dummy commit

--- trunk/code/cond_script.jl
+++ trunk/code/cond_script.jl
@@ -1,19 +1,22 @@
-#cd("../../code")
+cd("../../code")
 include("$(pwd())/PDispersion.jl")
-filename = ARGS[1]
 p = parse(Int64, ARGS[2])
 q = parse(Int64, ARGS[3])
 policy = ARGS[4]
-tok = split(filename, ['.', '/']; keepempty = false)
-ntok = length(tok)
-outfile = "$(tok[ntok - 1])_$(p)_$(q)_$policy.log"
+outfile = "../runs/cc2019/$(ARGS[1])_$(p)_$(q)_$policy.log"
 out = open(outfile, "w")
 
-PDispersion.read_instance_tsplib(filename)
+PDispersion.read_instance_tsplib("../instances/$(ARGS[1]).tsp")
 if policy == "greedy"
     nothing, Q = PDispersion.compute_lower_bound(q)
 elseif policy == "optimal"
+	PDispersion.set_maximum_time(21600)
     nothing, opt, nothing = PDispersion.pdispersion_decremental_clustering(q)
+	if !PDispersion.optimal()
+		println(out, "NA")
+		close(out)
+		exit(1)
+	end
     Q = PDispersion.get_coordinates(opt)
 elseif policy == "random"
     Q = PDispersion.get_random_coordinates(q)
@@ -23,18 +26,27 @@ lb0, nothing = PDispersion.compute_lower_bound(p)
 init_nnodes = PDispersion.get_nnodes()
 PDispersion.reduce_data_using_Q(lb0)
 final_nnodes = PDispersion.get_nnodes()
+PDispersion.set_maximum_time(86400)
+init_time = time_ns()
 lb, opt, groups = PDispersion.pdispersion_decremental_clustering(p)
-println(out, "Best $lb")
-println(out, "Initial $lb0")
+elapsed = round(Int64, (time_ns() - init_time) * 1e-8) * 1e-1
+println(out, "BestBound $lb")
+println(out, "Elapsed $elapsed")
+println(out, "InitialLB $lb0")
 println(out, "Eliminated $(init_nnodes - final_nnodes)")
 print(out, "Q")
 for i in 1 : q
     print(out, " $(Q[1, i]) $(Q[2, i])")
 end
 println(out)
-print(out, "Opt")
-optcoords = PDispersion.get_coordinates(opt)
-for i in 1 : p
-    print(out, " $(optcoords[1, i]) $(optcoords[2, i])")
+if PDispersion.optimal()
+	println(out, "Optimal y")
+	print(out, "BestSOL")
+	optcoords = PDispersion.get_coordinates(opt)
+	for i in 1 : p
+	    print(out, " $(optcoords[1, i]) $(optcoords[2, i])")
+	end
+	println(out)
+else println(out, "Optimal n")
 end
-println(out)
+close(out)
